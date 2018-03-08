3000264df4f8cd72ffb3a0d95ef054c1fc518e61
tanteuschi <maxi@openknowledgemaps.org>
2018-03-08 21:55:55 +0100

    community participation guidelines

---
+++ community-guidelines.php
@@ -0,0 +1,152 @@
+<?php include 'config.php' ?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <base href="<?php echo $SITE_URL ?>">
+        <?php $title = "FAQ - Open Knowledge Maps"; ?>
+        <?php include($COMPONENTS_PATH . 'head_bootstrap.php'); ?>
+        <?php include($COMPONENTS_PATH . 'head_standard.php'); ?>
+        <?php include($COMPONENTS_PATH . 'head_headstart.php') ?>
+
+    </head>
+    <body class="faq-page faqx">
+
+        <?php include($COMPONENTS_PATH . 'header.php'); ?>
+
+
+        <div id="news">  
+            <div class="headerimage"><img src="./img/faqx2.png"></div>
+            <div class="background2">
+                <div class="team">
+                    <p>Community participation guidelines</p>
+                    <p>Open Knowledge Maps is driven by people from all over the world, who are passionate about better 
+                        and more open solutions for discovery. Our goal is to create a respectful, sustainable and 
+                        inclusive environment for everyone involved. As such, we have adopted these guidelines, 
+                        which are aligned with our core values of openness, diversity, and common good.</p>
+                </div>
+            </div>
+
+            <div style="max-width:100%;">
+                <div class="faq">
+                    <p>We require all those who participate to agree and adhere to the Community Participation Guidelines in 
+                        order to help us create a safe and positive experience for all. These guidelines outline our behavior 
+                        expectations as members of the Open Knowledge Maps community, as well as the consequences for unacceptable 
+                        behavior. </p>
+                </div>
+                <div class="faq">
+                    <p class="faquestion">
+                        These guidelines apply in all Open Knowledge Maps activities, both offline and online, including but not 
+                        limited to:
+
+                    </p>
+                    <ul class="cgp">
+                        <li>Working in Open Knowledge Maps spaces.</li>
+                        <li>Working with other Open Knowledge Maps community participants virtually or co-located.</li>
+                        <li>Representing Open Knowledge Maps at public events.</li>
+                        <li>Representing Open Knowledge Maps in social media (official accounts, staff accounts, personal accounts, 
+                            Facebook pages).</li>
+                        <li>Participating in Open Knowledge Maps workshops and trainings.</li>
+                        <li>Participating in Open Knowledge Maps-related chats, mailing lists, documents, development environments 
+                            (e.g. Github), websites, group or person-to-person meetings.</li>
+                    </ul>
+                </div>
+                <div class="faq">
+                    <p>The Community Participation Guidelines aim to support a community where all people should feel safe, 
+                        regardless of sex, gender, gender identity, sexual orientation, disability, mental health, neuro(a)typicality, 
+                        physical appearance, body size, race, age, nationality, and political or religious affiliation.
+                    </p>
+                </div>
+
+                <div class="faq">
+                    <p class="faquestion">
+                        Encouraged Behaviour
+                    </p>
+                    <p>Examples of behavior that contributes to creating a positive environment include:
+                    </p>
+                    <ul class="cgp">
+                        <li>Using welcoming and inclusive language</li>
+                        <li>Being respectful of differing viewpoints and experiences</li>
+                        <li>Gracefully accepting constructive criticism</li>
+                        <li>Focusing on what is best for the community</li>
+                        <li>Showing empathy towards other community members</li>
+                    </ul>
+                </div>
+
+                <div class="faq">
+                    <p class="faquestion">
+                        Unacceptable Behaviour
+                    </p>
+                    <p>Examples of unacceptable behavior by participants include:
+                    </p>
+                    <ul class="cgp">
+                        <li>The use of sexualized language or imagery and unwelcome sexual attention or advances</li>
+                        <li>Trolling, insulting/derogatory comments, and personal or political attacks</li>
+                        <li>Public or private harassment</li>
+                        <li>Publishing others' private information, such as a physical or electronic address, without explicit 
+                            permission</li>
+                        <li>Other conduct which could reasonably be considered inappropriate in a professional setting</li>
+                    </ul>
+                </div>
+                <div class="faq">
+                    <p class="faquestion">
+                        Our Responsibilities
+                    </p>
+                    <p>
+                        Open Knowledge Maps team members are responsible for clarifying the standards of acceptable 
+                        behavior and are expected to take appropriate and fair corrective action in response to any instances 
+                        of unacceptable behavior.
+                    </p>
+                    <p>Open Knowledge Maps team members have the right and responsibility to ban temporarily or 
+                        permanently any participant for behaviors that they deem inappropriate, threatening, offensive, 
+                        or harmful and to remove, edit, or reject posts, comments, commits, code, document edits, issues, 
+                        and other contributions that are not aligned to the Community Participation Guidelines.
+                    </p>
+
+
+                </div>
+
+                <div class="faq">
+                    <p class="faquestion">
+                        Enforcement
+                    </p>
+                    <p>
+                        Instances of abusive, harassing, or otherwise unacceptable behavior may be reported by contacting 
+                        the Open Knowledge Maps team at <a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>. Please include information on who was 
+                        involved in the incident, what happened, and where and when it took place. All complaints will be 
+                        reviewed and investigated and will result in a response that is deemed necessary and appropriate to 
+                        the circumstances. The project team is obligated to maintain confidentiality with regard to the reporter 
+                        of an incident.
+                    </p>
+                    <p>Open Knowledge Maps team members who do not follow or enforce the Community Participation Guidelines 
+                        in good faith may face temporary or permanent repercussions as determined by other members of the 
+                        organisation’s leadership.
+                    </p>
+                    <p>
+                        If you feel you have been falsely or unfairly accused of violating this Code of Conduct, 
+                        you should notify the project team at <a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a> with a concise description of 
+                        your grievance.
+                    </p>
+
+                </div>
+                
+                <div class="faq">
+                    <p class="faquestion">
+                        Attribution
+                    </p>
+                    <p>These Community Participation Guidelines are adapted from:
+                    </p>
+                    <ul class="cgp">
+                        <li><a class="underline" target="_blank" href="http://contributor-covenant.org/version/1/4">atom Code of Conduct, available at</a></li>
+                        <li><a class="underline" target="_blank" href="https://www.mozilla.org/about/governance/policies/participation/">
+                            Mozilla Community Participation Guidelines, available at</a> 
+</li>
+<li><a class="underline" target="_blank" href="http://www.opencon2017.org/code_of_conduct">OpenCon Code of Conduct, available at</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+        <span class="anchor" id="newslettersignup"></span>
+        <?php include($COMPONENTS_PATH . 'newsletter.php'); ?>
+        <?php include($COMPONENTS_PATH . 'footer.php'); ?>
