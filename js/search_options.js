14222a288671e0e41b8e96ccd05df29b92ebc19c
Peter Kraker <pkraker@openknowledgemaps.org>
2018-03-12 11:48:06 +0100

    Update PubMed article types

--- js/search_options.js
+++ js/search_options.js
@@ -106,7 +106,8 @@ var options_pubmed = {
             ]},
         {id: "article_types", multiple: true, width: "140px", name: "Article types", type: "dropdown"
             , fields: [
-                {id: "addresses", text: "Addresses", selected: true}
+                {id: "adaptive clinical trial", text: "Adaptive Clinical Trial", selected: true}
+                , {id: "addresses", text: "Addresses", selected: true}
                 , {id: "autobiography", text: "Autobiography", selected: true}
                 , {id: "bibliography", text: "Bibliography", selected: true}
                 , {id: "biography", text: "Biography", selected: true}
@@ -135,7 +136,9 @@ var options_pubmed = {
                 , {id: "editorial", text: "Editorial", selected: true}
                 , {id: "electronic supplementary materials", text: "Electronic Supplementary Materials", selected: true}
                 , {id: "english abstract", text: "English Abstract", selected: true}
+                , {id: "equivalence trial", text: "Equivalence Trial", selected: true}
                 , {id: "ephemera", text: "Ephemera", selected: true}
+                , {id: "expression of concern", text: "Expression of Concern", selected: true}
                 , {id: "evaluation studies", text: "Evaluation Studies", selected: true}
                 , {id: "festschrift", text: "Festschrift", selected: true}
                 , {id: "government publications", text: "Government Publications", selected: true}
