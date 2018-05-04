e63c3e99aa6e4bacad1a7bec3500e1b6345e3df8
Peter Kraker <pkraker@openknowledgemaps.org>
2018-05-04 16:34:38 +0200

    Update Piwik/Matomo code

--- components/head_standard.php
+++ components/head_standard.php
@@ -79,20 +79,23 @@
 
 
 
-<?php if ($PIWIK_ENABLED) { ?>
-    <!-- Piwik -->
+<?php if ($PIWIK_ENABLED) { ?>  
+    
+    <!-- Matomo -->
     <script type="text/javascript">
       var _paq = _paq || [];
-      // tracker methods like "setCustomDimension" should be called before "trackPageView"
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
       _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
-        var u="<?php echo $SITE_URL . $PIWIK_PATH; ?>";
+        var u="<?php echo $SITE_URL . $PIWIK_PATH ?>";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
         _paq.push(['setSiteId', '1']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
       })();
     </script>
-    <!-- End Piwik Code -->
+    <noscript><p><img src="//openknowledgemaps.org/piwik_stats/piwik.php?idsite=1&rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
+    
 <?php }; ?>
