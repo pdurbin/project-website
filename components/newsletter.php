fc8272e008b9a56afbced67b421d8719e87b6059
tanteuschi <maxi@openknowledgemaps.org>
2018-05-23 23:03:34 +0200

    updates on impressum and newsletter signup form as required by GDPR

--- components/newsletter.php
+++ components/newsletter.php
@@ -1,32 +1,3 @@
-<style>
-    #mc_embed_signup div.mce_inline_error {
-        color: red;
-        background-color: white;
-        padding: 5px 0px;
-    }
-
-    #mc_embed_signup input.mce_inline_error {
-        border-color: red;
-    }
-    
-    #mce-error-response {
-        color: red;
-    }
-    
-    #mce-error-response a {
-        text-decoration: underline;
-    }
-    
-    #mce-success-response {
-        color: green;
-    }
-    
-    #mce-success-response a {
-        text-decoration: underline;
-    }
-    
-</style>
-
 <div id="newsletterdiv">
     <div id="signup-box">
     <div id="signup-pic"><img src="./img/newslettersignup2.png"></div>
@@ -36,51 +7,13 @@
         <form action="//openknowledgemaps.us13.list-manage.com/subscribe/post?u=c399f89442d6aa733a9896515&amp;id=3ff20c12f9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
             <h3>Sign-up for our Newsletter</h3>
             <p>Receive regular updates on the newest improvements.</p>
-            <div id="mc_embed_signup_scroll">
-                <div class="mc-field-group">
-                    <label for="mce-EMAIL">Email Address<span class="asterisk">*</span>
-                    </label>
-                    <input type="email" required value="" name="EMAIL" class="text-field" id="mce-EMAIL" size="53">
-                </div>
-                <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c399f89442d6aa733a9896515_3ff20c12f9" tabindex="-1" value=""></div>
-                <p style="font-size:10pt; margin: 10px 0px;">By clicking the button below, you subscribe to our newsletter. 
-                We use your e-mail address to send you periodic updates. 
-                You can unsubscribe at any time by clicking on a dedicated link in the newsletter, in which case we will also delete your data. 
-                We use a GDPR-compliant service such as MailChimp to manage your data, to send out e-mails and 
-                to conduct statistical analysis of newsletter use.
-                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="frontend-btn"></div>
-            </div>
 
-            <p id="alternate-signup" style="display:none"> 
-                <a href="http://eepurl.com/cjRDZL" target="_blank" class="underline"><i class="fa fa-envelope" aria-hidden="true"></i> To sign up, please use this link</a>
+            <p> 
+                <a href="http://eepurl.com/dvQeGP" target="_blank" class="underline"><i class="fa fa-envelope" aria-hidden="true"></i> To sign up, please use this link</a>
             </p>
             
         </form>
     </div>
     </div>
-    <script type='text/javascript' src='./lib/mc-validate.js'></script><script type='text/javascript'>(function ($) {
-            window.fnames = new Array();
-            window.ftypes = new Array();
-            fnames[0] = 'EMAIL';
-            ftypes[0] = 'email';
-            fnames[1] = 'FNAME';
-            ftypes[1] = 'text';
-            fnames[2] = 'LNAME';
-            ftypes[2] = 'text';
-        }(jQuery));
-        var $mcj = jQuery.noConflict(true);
-        
-        if(navigator.doNotTrack) {
-            $("#mc_embed_signup_scroll").css("display", "none");
-            $("#alternate-signup").css("display", "block");
-        }
-        
-    </script>
-
-    <!--End mc_embed_signup-->
 
 </div>
--- imprint.php
+++ imprint.php
@@ -14,17 +14,133 @@
 
 
         <div id="imprint">
-            <h2>Imprint</h2>
-            <p style="max-width:600px; margin:0px auto; text-align:center;">
-                <span class="bold">Name of association:</span> 
-                <br>Open Knowledge Maps - Verein zur Förderung <br>der Sichtbarkeit wissenschaftlichen Wissens
-                <br><br><span class="bold">Address:</span> 
-                <br>1030 Wien, Keilgasse 11/20 Austria
-                <br><br><span class="bold">Country:</span> 
-                <br>Austria
-                <br><br><span class="bold">Registration number (ZVR):</span> 
-                <br>859648888
-                <br><br><span class="bold">Email:</span> 
-                <br>info@openknowledgemaps.org</p>
+            <div style="max-width: 750px; width:100%; margin: 0px auto;">
+                <ul>
+                    <li><a class="underline" href="imprint#impressum">Impressum auf Deutsch</a></li>
+                    <li><a class="underline" href="imprint#legalnotice">Legal notice in English</a></li>
+                </ul>
+
+                <a name="impressum" class="anchor"></a><h2 style="text-align:left; margin-top: 30px;">Impressum</h2>
+                <p>Informationspflicht und Offenlegungspflicht laut §25 Mediengesetz.</p>
+
+                <h3>Name des Vereins</h3>
+                <p>Open Knowledge Maps - Verein zur Förderung
+                    der Sichtbarkeit wissenschaftlichen Wissens</p>
+
+                <h3>ZVR</h3>
+                <p>859648888</p>
+
+                <h3>Email</h3>
+                <p>info@openknowledgemaps.org</p>
+
+                <h3>Adresse</h3>
+                <p>Keilgasse 11/20, AT-1030 Wien</p>
+
+                <h3>Land</h3>
+                <p>Österreich</p>
+
+                <h3>Vereinszweck</h3>
+                <p>Der Verein bezweckt, wissenschaftliches Wissen für alle Mitglieder der Gesellschaft auffindbar und
+                    nutzbar zu machen und die daraus gewonnenen Erkenntnisse mit anderen zu teilen. Der Verein verfolgt 
+                    ausschließlich und unmittelbar gemeinnützige Ziele und ist nicht gewinnorientiert ausgerichtet. Der 
+                    Verein ist politisch und konfessionell unabhängig und handelt danach.</p>
+
+                <h3>Organschaftliche Vertreter</h3>
+                <p>Obmann: Dr. Peter Kraker<br>
+                    Kassierin: Maxi Schramm<br>
+                    Schriftführer: Christopher Kittel<br>
+                </p>
+                
+                <h3>Gerichtsstand</h3>
+                <p>Gerichtsstand bei Rechtsstreitigkeiten, die aus der Nutzung dieser Website resultieren, ist Wien. 
+                    Es gelangt in allen Fällen österreichisches Recht zur Anwendung.</p>
+
+                <h3>Haftungsauschluss</h3>
+                <p>Open Knowledge Maps betreibt diese Website um es der Öffentlichkeit zu ermöglichen, wissenschaftliches Wissen zu finden und wiederzuverwenden. Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und aktuelle
+                    Informationen bereitzustellen. Leider können wir keine Haftung für die Korrektheit aller Inhalte
+                    auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt werden.
+                    Sollten Ihnen problematische oder rechtswidrige Inhalte auffallen, bitten wir Sie uns
+                    umgehend zu kontaktieren. Sie finden die Kontaktdaten im Impressum.
+                </p>
+
+                <p>Unsere Webseite enthält Links zu anderen Webseiten für deren Inhalt wir nicht verantwortlich sind.
+                    Wenn Ihnen rechtswidrige Links auf unserer Webseite auffallen, bitte wir Sie uns zu kontakieren,
+                    Sie finden die Kontaktdaten im Impressum.</p>
+
+                <h3>Urheberrechtshinweis</h3>
+                <p>Falls nicht anders gekennzeichnet, unterliegen alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) der Lizenz <a class="underline" target="_blank" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a> mit Urheber Open Knowledge Maps. Ansonsten gilt die beim jeweiligen Werk angegebene Lizenz
+                    und der angegebene Urheber.</p>
+                <p>Unsere Software ist open source und wird auf Github gehosted.
+                </p>
+                <p>Sollten Sie auf dieser Webseite Inhalte finden, die das Urheberrecht verletzen, bitten wir Sie uns zu kontaktieren.
+                </p>
+
+                <h3>Bildnachweis</h3>
+                <p>Die Bilder, Fotos und Grafiken auf dieser Webseite sofern nicht anders gekennzeichnet unterliegen der Lizenz:
+                    <a class="underline" target="_blank" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. Falls nicht anders angegeben, ist der Urheber Open Knowledge Maps.
+                    Ansonsten sind der Urheber und die Lizenz beim Werk entsprechend vermerkt.</p>
+
+                <h3>Quelle</h3>
+                <p>Die Texte für das Impressum wurden mit Hilfe folgender Quellen erstellt:<br>
+                    https://www.adsimple.at/impressum-generator/impressum-generator-text/</p>
+
+                <a name="legalnotice" class="anchor"></a>
+                <h2 style="margin-top: 50px; text-align:left;">Legal Notice</h2>
+
+                <h3>Name of association</h3>
+                <p>Open Knowledge Maps - Verein zur Förderung
+                    der Sichtbarkeit wissenschaftlichen Wissens</p>
+
+                <h3>Registration number (ZVR)</h3>
+                <p>859648888</p>
+
+                <h3>Email</h3>
+                <p>info@openknowledgemaps.org</p>
+
+                <h3>Address</h3>
+                <p>Keilgasse 11/20, AT-1030 Vienna</p>
+
+                <h3>Country</h3>
+                <p>Austria</p>
+
+                <h3>Purpose of the association</h3>
+                <p>The purpose of the association is to enable all members of society to find and reuse scientific knowledge, 
+                    and to share the insights gained in this process with others. The association exclusively and directly 
+                    pursues charitable objectives and is a non-profit. The association is politically and confessionally 
+                    independent and acts accordingly.</p>
+
+                <h3>Open Knowledge Maps Board</h3>
+                <p>Chairman: Dr. Peter Kraker<br>
+                   Treasurer: Maxi Schramm<br>
+                   Secretary: Christopher Kittel<br>
+                </p>
+                
+                <h3>Place of jurisdiction</h3>
+                <p>Place of jurisdiction for all legal disputes arising from the use of this website is Vienna, Austria. Austrian law applies.</p>
+
+
+                <h3>Disclaimer</h3>
+                <p>Open Knowledge Maps maintains this website to enable the public to find and reuse scientific knowledge. (more info see our purpose of association)
+                    Our goal is to keep this information timely and accurate. If errors are brought to our attention, we will try to correct them.
+                    However, Open Knowledge Maps accepts no responsibility or liability whatsoever with regard to the information on this site.
+                </p>
+
+                <p>Open Knowledge Maps is not responsible for the contents of websites that are maintained by third parties and
+                    therefore waives its liability for any links from this website to other websites.</p>
+
+                <h3>Licenses</h3>
+                <p>Unless otherwise noted, all content on openknowledgemaps.org is licensed under a <a class="underline" target="_blank" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+                    Our software is open source and hosted on Github.
+                    Otherwise, the copyright holder and and the license are specified in/with the work.</p>
+
+                <p>Should you find any content on this website that is not attributed correctly please notify us (see contact details above).
+                </p>
+
+                <h3>Picture credits</h3>
+                <p>Unless otherwise noted, all pictures, photographs and graphics on this website is licensed under a
+                    <a class="underline" target="_blank" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. Unless otherwise noted, Open Knowledge Maps is the copyright holder.
+                    Otherwise, the copyright holder and and the license are specified in/with the work.</p>
+
+            </div>
         </div>
         <?php include($COMPONENTS_PATH . 'footer.php'); ?>
