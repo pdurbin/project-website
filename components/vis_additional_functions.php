568dc071a3a15b7445a43b84be6340ab9f490e0a
Peter Kraker <pkraker@openknowledgemaps.org>
2018-07-01 22:06:14 +0200

    Fixed remaining issues with modal scrolling & resizing

--- components/vis_additional_functions.php
+++ components/vis_additional_functions.php
@@ -26,6 +26,7 @@
         <?php if (!isset($_GET['embed']) || $_GET['embed'] === 'false'): ?>
             var absolute_left = $("#visualization").offset().left;
             var offset = 0;
+            var top_pos = $("#modals").position().top;
 
             $(".close").click(function(){
                 $("#modals").css("top", $("#modals").position().top - $('#desktop-warning').outerHeight());
@@ -37,24 +38,15 @@
             <?php if (!isset($_GET['embed']) || $_GET['embed'] === 'false'): ?>
                 let previous_div_height = div_height;
                 div_height = calcDivHeight();
-                
-                let ratio = previous_div_height/div_height;
 
                 $(".overflow-vis").css("min-height", div_height + "px")
                 $("#visualization").css("min-height", div_height + "px")
                 
                 absolute_left = $("#visualization").offset().left;
 
-                //console.log("div_height: " + div_height + "\ntop_position: " + $("#modals").position().top);
                 $("#modals").css("position", "fixed");
-                /*if(($(window).scrollTop() > div_height - $("#modals").position().top) && $("#modals").position().top > 0) {
-                    //$("#modals").css("top",  div_height - $(window).scrollTop() + ($("#headstart-chart").position().top - $("#visualization").position().top));
-                    //$("#modals").css("top",  div_height - $("#headstart-chart").position().top);
-                } else {
-                     //$("#modals").css("top",  $(window).scrollTop())
-                }*/
                 $("#modals").css("left",  absolute_left + offset);
-                $("#modals").followTo(div_height, $("#modals").position().top, absolute_left, offset);
+                $("#modals").followTo(div_height, top_pos, absolute_left, offset);
             <?php endif ?>
         });
 
