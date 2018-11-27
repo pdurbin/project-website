18cffcdf172412c86d67456087f18424c0eea3d3
tanteuschi <maxi@openknowledgemaps.org>
2018-11-27 16:29:04 +0100

    updates project page: section for index.php, project images, bg image, css

---
+++ components/your-collection.php
@@ -0,0 +1,47 @@
+
+<div id="mission">
+    <a name="your-collection"></a>
+    
+    <div class="faq">
+        <h2 class="h2reverse">How to work with us</h2>
+        <p style="text-align:center;">Open Knowledge Maps is a charitable non-profit.
+                        At OKMaps our aim is to improve the visibility of scientific knowledge, that’s why we partner with organizations that 
+                        share our goals to develop innovative projects. <a class="underline" href="./projects">Find out more</a>.</p>
+    </div>
+    <div id="more-info">
+        <a target="_blank" href="">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/project-examples/viper.png"></p>
+                <h2>VIPER - the visual project explorer</h2>
+                <p>A visualization of project outputs as indexed in the OpenAIRE  database.</p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+
+            </div>
+        </a>
+        
+        <a target="_blank" href="">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/project-examples/crisvis.png"></p>
+                <h2>CRIS I visualization</h2>
+                <p>A visualization of research questions</p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+
+            </div>
+        </a>
+        
+        <a href="./projects">
+            <div class="vision" style="min-height: 20px;">
+                <p><img src="./img/project-examples/yourdatacollection.png"></p>
+                <h2>A visualization of your data set</h2>
+                <p>Find out more on how to work with us!</p>
+                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+
+            </div>
+        </a>
+    </div>
+    
+    <div class="try-now">
+            <a class="become-enthusiast" href="./projects">Collaborate with us!</a>
+        </div>
+
+</div>
--- projects.php
+++ projects.php
@@ -9,29 +9,28 @@
         <?php include($COMPONENTS_PATH . 'head_headstart.php') ?>
     </head>
 
-    <body class="team-page projects-page">
+    <body class="projects-page">
 
         <?php include($COMPONENTS_PATH . 'header.php'); ?>
 
         <div id="team">
-            <div class="headerimage"><img src="./img/"></div>
             <div class="background2 bg2">
                 <div class="team">
                     <p>Projects</p>
                     <p>Open Knowledge Maps is a charitable non-profit.
-At OKMaps our aim is to improve the visibility of scientific knowledge, that’s why we partner with organizations that 
-share our goals to develop innovative projects. <a class="underline" href="./getintouch">Get in touch</a>.</p>
+                        At OKMaps our aim is to improve the visibility of scientific knowledge, that’s why we partner with organizations that 
+                        share our goals to develop innovative projects. <a class="underline" href="./getintouch">Get in touch</a>.</p>
                 </div>
             </div>
         </div>
-        
-        <?php  include($COMPONENTS_PATH . "project-examples.php"); ?>
+
+        <?php include($COMPONENTS_PATH . "project-examples.php"); ?>
         <?php
         $COMMENT_TITLE = "What our project partners say";
         $COMMENT_IMAGES_URL = "./img/project-partners/partner-statements/";
         include($COMPONENTS_PATH . 'commentstream.php');
         ?>            
-        <?php  include($COMPONENTS_PATH . "project-work-with-us.php"); ?>
-       <?php  include($COMPONENTS_PATH . "project-team.php"); ?>
-        <?php  include($COMPONENTS_PATH . "newsletter.php"); ?>
+        <?php include($COMPONENTS_PATH . "project-work-with-us.php"); ?>
+        <?php include($COMPONENTS_PATH . "project-team.php"); ?>
+        <?php include($COMPONENTS_PATH . "newsletter.php"); ?>
         <?php include($COMPONENTS_PATH . 'footer_base.php'); ?>
