3b81b201e766074de8b0fd30cea863131e2d0fe2
tanteuschi <maxi@openknowledgemaps.org>
2018-12-04 22:51:39 +0100

    thankyou image mobile, css fixes on thankyou and donation page

--- thank-you.php
+++ thank-you.php
@@ -9,7 +9,7 @@
         <?php include($COMPONENTS_PATH . 'head_headstart.php') ?>
     </head>
 
-    <body class="about-page">
+    <body class="thankyou-page">
 
         <?php include($COMPONENTS_PATH . 'header.php'); ?>
         <script>
@@ -17,6 +17,7 @@
         </script>
 
         <div id="about-page">
+            
             <div class="background2">
                 <div class="team">
                     <p style="text-align:center;"><img src="./img/donationthankyou2.gif"></p>
@@ -25,21 +26,20 @@
                         If you want to receive a receipt for your donation, please send us an 
                         e-mail with your name and address to <br>
                         <a  class="underline" target="_blank" href="mailto:donations@openknowledgemaps.org">donations@openknowledgemaps.org</a>.
-                        
-                        
                     </p>
-                     <p style="margin-top: 20px;">
-                         With your donation you are helping people from all around 
-                the world to discover scientific knowledge.
-                         
-            </p>
+                    <p style="margin-top: 20px;">
+                        With your donation you are helping people from all around 
+                        the world to discover scientific knowledge:
+                    </p>
                 </div>
             </div>
-            
-           
-            
-            <div style="text-align: center;">
+
+            <div class="thank-you-desktop" style="text-align: center;">
                 <img class="shadow" src="./img/thankyou-donation.jpg">
+            </div>
+
+            <div class="thank-you-mobile">
+                <img class="shadow" src="./img/thankyou-donation-mobile.jpg">
             </div>
 
         </div>
