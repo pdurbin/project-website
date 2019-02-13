a140f1dde4be04dea87c19472295819af6196100
tanteuschi <maxi@openknowledgemaps.org>
2019-02-13 16:56:37 +0100

    small fixes

--- components/supporting-members.php
+++ components/supporting-members.php
@@ -2,8 +2,6 @@
 <div id="partnerdiv" style="border-top: 1px solid #cacfd3;">
     <h2>Our Supporting members</h2>
     <div class="partners">
-
         <a href="https://lbg.ac.at/" target="_blank"><img src="./img/partners/lbg-oisc.png" alt="LBG"></a>
-
     </div>
 </div>       
