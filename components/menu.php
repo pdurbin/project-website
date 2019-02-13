7a4b796d279033a19ec3e53c9e9a933f37a1aee7
tanteuschi <maxi@openknowledgemaps.org>
2019-02-13 15:32:09 +0100

    mobile menu fixes

--- components/menu.php
+++ components/menu.php
@@ -7,15 +7,15 @@
 
     <!-- Mobile Menu -->
     <ul>
-        <a href="index"><li>Search</li></a>
-        <a href="about"><li>About</li></a>
-        <a href="team"><li>Team</li></a>
-        <a href="community"><li>Community</li></a>
-        <a href="projects"><li>Projects</li></a>
-        <a href="news"><li>News</li></a>
-        <a href="faq"><li>FAQs</li></a>
-        <a href="getintouch"><li>Get in touch</li></a>
-        <a href="donate-now" style="color: #f29e00;"><li><i class="fa fa-heart" aria-hidden="true"></i> Support us</li></a>
+        <li><a href="index">Search</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="team">Team</a></li>
+        <li><a href="community">Community</a></li>
+        <li><a href="projects">Projects</a></li>
+        <li><a href="news">News</a></li>
+        <li><a href="faq">FAQs</a></li>
+        <li><a href="getintouch">Get in touch</a></li>
+        <li><a href="donate-now" style="color: #f29e00;"><i class="fa fa-heart" aria-hidden="true"></i> Support us</a></li>
         <!--<a href="index#supportus" style="color: #E55137;"><li><i class="fa fa-heart" aria-hidden="true"></i> Support us</li></a>-->
     </ul>
 </div>
@@ -34,7 +34,6 @@
 </ul>
 
 <ul class="nav_top">
-
     <li><a href="index">Search</a></li>
     <li><a href="about">About</a></li>
     <li><a href="team">Team</a></li>
