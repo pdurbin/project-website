a3ed6823ef415894e4af0aab84a3c77573fca09e
tanteuschi <maxi@openknowledgemaps.org>
2019-02-13 14:58:21 +0100

    newsletter signup fixes

--- components/newsletter.php
+++ components/newsletter.php
@@ -1,7 +1,7 @@
 <div id="newsletterdiv">
     <div id="signup-box">
-    <div id="signup-pic"><img src="./img/newslettersignup2.png"></div>
-    <div id="signup-pic-mobil"><img src="./img/newslettersignup-mobil.png"></div>
+        <div id="signup-pic"><a href="http://eepurl.com/dvQeGP" target="_blank"><img src="./img/newslettersignup2.png"></a></div>
+        <div id="signup-pic-mobil"><a href="http://eepurl.com/dvQeGP" target="_blank"><img src="./img/newslettersignup-mobil.png"></a></div>
 
     <div id="mc_embed_signup">
         <form action="//openknowledgemaps.us13.list-manage.com/subscribe/post?u=c399f89442d6aa733a9896515&amp;id=3ff20c12f9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
